5ccfcf906838afb08ae58c9837dcf093a74a6bc5
DielmeRamosGomes <dielme@unesp.br>
2025-03-19 09:23:39 -0300

    atualizar

--- fluxo-de-pagamento/fpagamento.py
+++ fluxo-de-pagamento/fpagamento.py
@@ -1,12 +1,13 @@
-pagamento = "CARTAO_CREDITO"
+pagamento = "CARTAO_DEBITO"
 opcao = "VISTA"
+valor = "300"
 if pagamento == "CARTAO_CREDITO":
     if opcao == "VISTA":
         print("Pagamento feito com cartao de credito a vista")
     elif opcao == "PARCELADO":
         print("Pagamento parcelado")
 elif pagamento == "CARTAO_DEBITO":
-    print("Pagamento feito com débito")
+    print("Pagamento feito com débito e o valor é : "+ valor)
 elif pagamento == "PIX":
     if opcao == "VISTA":
         print("Pix feito a vista")
