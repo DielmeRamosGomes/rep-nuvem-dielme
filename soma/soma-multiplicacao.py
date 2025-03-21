c2201bfb5168e83d9e44856ca1ed8f36eb8ebc70
DielmeRamosGomes <dielme.ramos@unesp.br>
2025-03-21 09:12:42 -0300

    atualizando

---
+++ soma/soma-multiplicacao.py
@@ -0,0 +1,17 @@
+numero1 = float(input("Digite um numero: "))
+numero2 = float(input("Digite outro numero: "))
+opcao = input("digite uma operação: ")
+resultado = 0
+
+if opcao == "+":
+     resultado =  numero1 + numero2
+     print(resultado)
+elif opcao == "*":
+     resultado = numero1 * numero2
+     print(resultado)
+else:
+    print("Não foi possivel fazer a operação")
+
+
+
+
