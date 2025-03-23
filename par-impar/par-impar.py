2519ff8eb429ce21f8d108fe512fce72c0ad2a9e
DielmeRamosGomes <dielme@unesp.br>
2025-03-23 00:36:04 -0300

    atualizando codigos

---
+++ par-impar/par-impar.py
@@ -0,0 +1,16 @@
+par = int(input("Digite um número: "))
+
+if par % 2 == 0:
+    print("O número é par")
+else:
+    print("O número é ímpar")
+
+
+
+
+
+
+
+
+
+
---
+++ soma/soma.py
@@ -0,0 +1,9 @@
+numero1 = float(input("Digite um numero: "))
+numero2 = float(input("Digite outro numero: "))
+
+if numero1 >= numero2:
+    resultado = numero1 + numero2
+else:
+    resultado = numero2 - numero1
+print(f"Resultado é = {resultado}")
+
