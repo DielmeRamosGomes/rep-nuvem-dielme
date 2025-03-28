5be1305f8c5946114fc69ac4f651c3b5760ae825
DielmeRamosGomes <dielme@unesp.br>
2025-03-27 21:27:54 -0300

    atualização estrutura de dados

--- estrutura-de-dados/listas.py
+++ estrutura-de-dados/listas.py
@@ -1,3 +1,6 @@
-
-
+lista = [1, 2, 3, 4, 5]
+print(lista) # Imprime a lista completa
+print(f"lista[0] = {lista[0]}") # Imprime o primeiro elemento da lista
+ # Adiciona o elemento 6 à lista
+print(lista) # Adiciona o elemento 6 à lista
 
