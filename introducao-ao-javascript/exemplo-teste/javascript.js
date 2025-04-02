77dd0b486b67b5d4b350f1deb0312acc68e3d54e
DielmeRamosGomes <dielme@unesp.br>
2025-04-01 22:34:13 -0300

    atualização-sql

---
+++ introducao-ao-javascript/exemplo-teste/javascript.js
@@ -0,0 +1,41 @@
+import { createInterface } from 'readline';
+const rl1 = createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let numero;
+let resultado;
+let numeros = [];
+
+function lerNumeros() {
+    rl1.question(`Digite um número: `, (entrada) => {
+        numero = parseFloat(entrada);
+        if(isNaN(numero)) {
+            console.log("Entrada inválida. Por favor, insira um número."); 
+            //rl1.close();
+        }
+        else {
+            numeros.push(numero);
+            if(numeros.length < 2) {
+                lerNumeros();
+            }
+            else{
+                if (numeros[0] >= numeros[1]) {
+                    resultado = numeros[0] + numeros[1];
+                    console.log(`O resultado da soma é = ${resultado}`);
+                }
+                else {
+                    resultado = numeros[1] - numeros[0];
+                    console.log(`O resultado da subtração é = ${resultado}`);
+                }
+
+            }
+        }
+    });
+}
+    
+lerNumeros();
+
+
+
