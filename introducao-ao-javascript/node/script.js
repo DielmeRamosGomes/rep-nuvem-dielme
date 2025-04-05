d290f52952f04729a5bab19053718ce8b48e08ce
DielmeRamosGomes <dielme@unesp.br>
2025-04-04 23:04:29 -0300

    atualizando

--- introducao-ao-javascript/node/script.js
+++ introducao-ao-javascript/node/script.js
@@ -1,11 +1,35 @@
-let numero = 1;
+/*let numero = 1;
 
 while (numero <= 5) {
     console.log(`numero = ${numero}`);
     numero++; // numero = numero + 1;
 }
+*/
 
-
+/*
+for (let cont = 0 ; cont < 5; cont++) {
+    console.log(`cont = ${cont}`);
+}
+*/
+/*
+let cont = 0;
+do {
+    console.log(`cont = ${cont}`);
+    cont++;
+} while (cont < 5);
+*/
+/*
+let lista = [0, 1, 2, 3, 4];
+lista.forEach(element => {
+    console.log(`element = ${element}`);
+});
+*/
+/*
+let lista = [0, 1, 2, 3, 4];
+for (let element in lista) {
+    console.log(`element = ${element}`);
+}
+*/
 
 
 
