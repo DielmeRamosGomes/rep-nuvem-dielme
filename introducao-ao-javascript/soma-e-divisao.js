d8ffcb1a59cf5c718d4910fb47606ec9454312e0
DielmeRamosGomes <dielme@unesp.br>
2025-04-04 22:29:58 -0300

    javascript

--- introducao-ao-javascript/soma-e-divisao.js
+++ introducao-ao-javascript/soma-e-divisao.js
@@ -1,18 +1,33 @@
-let n_vezes = 3;
-let a = 1;
-let b = n_vezes;
-let resultado = 0;
-contador_a = 1;
-contador_b = n_vezes;
+import {createInterface} from 'readline';
 
-while (contador_a <= n_vezes) {
-    resultado = resultado + (a/b);
-    contador_a = contador_a + 1;
-    contador_b = contador_b - 1;
-    a = contador_a;
-    b = contador_b;
-}
-console.log(`Resultado = ${resultado}`);
+const readLine = createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+readLine.question(`Digite o número de vezes que deseja fazer a soma e divisão: `,(entrada) => {
+    let n_vezes = parseInt(entrada);
+    if (isNaN(n_vezes)) {
+        console.log(`Entrada inválida!`);
+        readLine.close();
+        return;
+    }
+    let a = 1;
+    let b = n_vezes;
+    let resultado = 0;
+    contador_a = 1;
+    contador_b = n_vezes;
+
+    while (contador_a <= n_vezes) {
+        resultado = resultado + (a/b);
+        contador_a = contador_a + 1;
+        contador_b = contador_b - 1;
+        a = contador_a;
+        b = contador_b;
+    }
+    console.log(`Resultado = ${resultado}`);
+    readLine.close();
+});
 
 
 
