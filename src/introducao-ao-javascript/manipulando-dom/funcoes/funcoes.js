f9d15e7704a5304c59b6300158e1d7a8fd503ac0
DielmeRamosGomes <dielme@unesp.br>
2025-04-06 20:14:08 -0300

    atualizando funcoes

---
+++ src/introducao-ao-javascript/manipulando-dom/funcoes/funcoes.js
@@ -0,0 +1,30 @@
+let numero1 = 10;
+let numero2 = 20;
+/*
+function somar(num1, num2) {
+    return num1 + num2;
+}
+// 10 + 20 = 30
+console.log(`A soma de ${numero1} + ${numero2} = ${somar(numero1, numero2)}`);
+*/
+/*
+const soma = function (num1, num2) {
+    return num1 + num2;
+}
+console.log(`A soma de ${numero1} + ${numero2} = ${soma(numero1, numero2)}`);
+*/
+/*
+const soma = (num1, num2) => {
+    return num1 + num2;
+}
+console.log(`A soma de ${numero1} + ${numero2} = ${soma(numero1, numero2)}`);
+*/
+/*
+const soma = (num1, num2) => num1 + num2;
+console.log(`A soma de ${numero1} + ${numero2} = ${soma(numero1, numero2)}`);
+*/
+/*
+(function (num1, num2) {
+    console.log(`A soma de ${numero1} + ${numero2} = ${num1 + num2}`);
+})(numero1, numero2);
+*/
