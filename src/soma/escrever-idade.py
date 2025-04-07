fc319d5881ec16d5add284ef0686617c73e4cf2d
Dielme Ramos Gomes <dielme@professor.educacao.sp.gov.br>
2025-04-07 09:31:03 -0300

    atualizando escrever-idade

---
+++ src/soma/escrever-idade.py
@@ -0,0 +1,12 @@
+nome = input(f"Digite um nome: ")
+idade = int(input(f"Digite uma idade: "))
+anoAtual = 2025
+anoDeNascimento = anoAtual - idade
+print(f"Seu nome é = {nome}")
+print(f"Ano de nascimento = {anoDeNascimento}")
+
+
+
+
+
+
