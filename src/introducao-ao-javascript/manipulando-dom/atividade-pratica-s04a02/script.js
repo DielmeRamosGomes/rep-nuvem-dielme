56b40499000e95abbff55755b3baeaa6416f4a26
Dielme Ramos Gomes <dielme@professor.educacao.sp.gov.br>
2025-04-07 10:39:03 -0300

    atualizar pfe

--- src/introducao-ao-javascript/manipulando-dom/atividade-pratica-s04a02/script.js
+++ src/introducao-ao-javascript/manipulando-dom/atividade-pratica-s04a02/script.js
@@ -1,7 +1,12 @@
 const botao = document.getElementById('botao');
+const botaoVolta = document.getElementById('botao-volta');
 const paragrafo = document.getElementById('paragrafo');
 
 botao.addEventListener('click', () => {
   paragrafo.textContent = 'Parágrafo Alterado!';
+});
+
+botaoVolta.addEventListener('click', () => {
+  paragrafo.textContent = 'Olá bem-vindo ao site!';
 });
 
