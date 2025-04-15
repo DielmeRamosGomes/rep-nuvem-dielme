be00eac6f28157b77d9f6cedf9fd3ea47b95e884
Dielme Ramos Gomes <dielme@professor.educacao.sp.gov.br>
2025-04-15 08:05:38 -0300

    atualizando rep-local

--- src/python-pbe/ola.py
+++ src/python-pbe/ola.py
@@ -1,5 +1,9 @@
+import requests
+# pip install requests
 
-for cont in range(5):
-    print(f"cont = {cont}")
+nome_cidade = input('Nome da cidade Ex Sao_Paulo: ')
 
-
+req = requests.get(f'https://api.open-meteo.com/v1/forecast?latitude=-23.5475&longitude=-46.6361&hourly=temperature_2m&timezone=America%2F{nome_cidade}&forecast_days=1')
+print(req.status_code)
+# print(req.json())
+print(req.json(['temperature_2m'][0]))
