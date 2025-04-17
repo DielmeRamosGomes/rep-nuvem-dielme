7d6eece1e04e2b942f189be12647cd03f0d89550
Dielme Ramos Gomes <dielme@professor.educacao.sp.gov.br>
2025-04-17 10:55:31 -0300

    atualizando vite-jogo-da-memoria

---
+++ src/segundo-bimestre/pfe/react/vite-jogo-da-memoria/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Card_image from './components/card-image/Card_image.jsx';
+
+function App() {
+  return(
+    <>
+      <div className='container'>
+        <Card_image/>
+      </div>
+    </>
+  );
+}
+export default App;
---
+++ src/segundo-bimestre/pfe/react/vite-jogo-da-memoria/src/components/card-image/Card_image.jsx
@@ -0,0 +1,15 @@
+
+function Card_image() {
+    return(
+        <>
+            <div className="card-image">
+               <img src="/image/gato1.jpg" alt="" /> 
+            </div>
+        </>
+    );
+}
+export default Card_image;
+
+
+
+
