b7d88393cf9fb3de5d54b35d022becf8b63dec6c
DielmeRamosGomes <dielme.ramos@unesp.br>
2025-04-23 20:37:47 -0300

    atualiza jogo-da-memoria

--- src/segundo-bimestre/pfe/react/vite-jogo-da-memoria/src/App.jsx
+++ src/segundo-bimestre/pfe/react/vite-jogo-da-memoria/src/App.jsx
@@ -1,13 +1,13 @@
-import './App.css'
+import React from 'react';
 import Card_image from './components/card-image/Card_image.jsx';
 
 function App() {
   return(
-    <>
-      <div className='container'>
-        <Card_image/>
-      </div>
-    </>
+    <div className='container'>
+      <Card_image/>
+      <br/>
+      <Card_image/>
+    </div>
   );
 }
 export default App;
--- src/segundo-bimestre/pfe/react/vite-jogo-da-memoria/src/components/card-image/Card_image.jsx
+++ src/segundo-bimestre/pfe/react/vite-jogo-da-memoria/src/components/card-image/Card_image.jsx
@@ -1,11 +1,11 @@
+import gato1 from './image/gato1.jpg';
+import './Card_image.css';
 
 function Card_image() {
     return(
-        <>
-            <div className="card-image">
-               <img src="/image/gato1.jpg" alt="" /> 
-            </div>
-        </>
+        <div className="card-image">
+            <img id='gato1' src={gato1} alt=""/> 
+        </div>
     );
 }
 export default Card_image;
