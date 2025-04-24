2c03aec1eeeeb9a564cafbc30411fc4d6a66c5af
DielmeRamosGomes <dielme.ramos@unesp.br>
2025-04-24 19:01:49 -0300

    atualizar sql

--- src/segundo-bimestre/mbd/clientes.sql
+++ src/segundo-bimestre/mbd/clientes.sql
@@ -1,16 +1,16 @@
 create database bd_clientes;
 
 create table if not exists clientes(
-    cliente_id int auto_increment primary key,
-    cliente_nome varchar(100) not null,
-    cliente_idade int not null
+    id int auto_increment primary key,
+    nome varchar(100) not null,
+    idade int not null
 );
 
-insert into clientes(cliente_id, cliente_nome, cliente_idade)
+insert into clientes(id, nome, idade)
     values(default, "Carlos", 23);
 
 select * from clientes;
-select cliente_nome, cliente_idade from clientes;
+select nome, idade from clientes;
 
 
 
