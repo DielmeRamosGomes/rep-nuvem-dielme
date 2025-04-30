443e32215d84a2739be59e4b3bba2786d0ee9643
DielmeRamosGomes <dielme.ramos@unesp.br>
2025-04-30 10:08:32 -0300

    atualizar processos

--- src/segundo-bimestre/llp/llp-s9a2/candidato.py
+++ src/segundo-bimestre/llp/llp-s9a2/candidato.py
@@ -1,18 +1,20 @@
 class Candidato:
-    def __init__(self, cand_numero, cand_partido, cand_nome, cand_vice):
+    def __init__(self, cand_numero, cand_partido, cand_nome, cand_vice, cand_cargo):
         self.cand_numero = cand_numero
         self.cand_partido = cand_partido
         self.cand_nome = cand_nome
         self.cand_vice = cand_vice
+        self.cand_cargo = cand_cargo
 
     def exibe_canditado(self):
         print(f"Numero: {self.cand_numero}")
         print(f"Partido: {self.cand_partido}")
         print(f"Nome: {self.cand_nome}")
+        print(f"Cargo do Candidato: {self.cand_cargo}")
         print(f"Nome do Vice: {self.cand_vice}")
 
-canditato1 = Candidato(1, "Partido B", "Carlos", "Maria")
-canditato2 = Candidato(2, "Partido C", "Joao", "Igor")
+canditato1 = Candidato(1, "Partido B", "Carlos", "Maria", "Prefeito")
+canditato2 = Candidato(2, "Partido C", "Joao", "Igor", "Governador")
 canditato2.exibe_canditado()
 
 
