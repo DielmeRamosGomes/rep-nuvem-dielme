48fad61e81f4d6b36f069f8f95e07adf31c8b561
DielmeRamosGomes <dielme.ramos@unesp.br>
2025-05-12 09:29:59 -0300

    atualizar

--- src/segundo-bimestre/llp/llp-s10a1/tabuada.py
+++ src/segundo-bimestre/llp/llp-s10a1/tabuada.py
@@ -1,6 +1,6 @@
-while input("Deseja continuar? (s/n): ").lower() != 'n':
+while input("Deseja continuar? (sim/nao): ").lower() != 'nao':
     numero = int(input("Digite um número: "))
-    print(f"Tabuadas do numero: {numero}")
+    print(f"Tabuada do numero: {numero}")
     for i in range(1, 11):
         print(f"{numero} x {i} = {numero * i}")
 
