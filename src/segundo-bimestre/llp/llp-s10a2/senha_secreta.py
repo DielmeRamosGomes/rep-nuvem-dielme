a1820926c69b10afa7a334143dc8b796379643dc
DielmeRamosGomes <dielme.ramos@unesp.br>
2025-05-16 09:29:41 -0300

    atualizar

--- src/segundo-bimestre/llp/llp-s10a2/senha_secreta.py
+++ src/segundo-bimestre/llp/llp-s10a2/senha_secreta.py
@@ -5,8 +5,17 @@ while input("Deseja executar novamente?[sim, nao]:  ").lower() != "nao":
     senha = input("Digite a senha secreta: ")
     caracteres = string.ascii_letters + string.digits
     string_aleatoria = ''.join(random.choice(caracteres) for i in range(senha)) 
+    numero_tentativas = 1
     while senha != string_aleatoria:
-
+        if numero_tentativas > 5:
+            break
+        numero_tentativas = numero_tentativas + 1
+    if numero_tentativas > 5:
+        print("Você perdeu!")
+    else:
+        print("Você ganhou!")
+        print(f"A senha secreta era: {senha}")
+        print(f"A senha gerada foi: {string_aleatoria}")
 
 
 
