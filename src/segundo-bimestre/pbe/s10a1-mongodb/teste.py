e8d5fd3e3b5fcc57b74ff775b52142341190f530
DielmeRamosGomes <dielme.ramos@unesp.br>
2025-05-20 21:34:23 -0300

    atualizando mongo db

--- src/segundo-bimestre/pbe/s10a1-mongodb/teste.py
+++ src/segundo-bimestre/pbe/s10a1-mongodb/teste.py
@@ -3,7 +3,7 @@ import pymongo
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://dielme:<db_password>@cluster0.g8uqx.mongodb.net/"
+uri = "mongodb+srv://dielme:1234@cluster0.g8uqx.mongodb.net/"
 client = MongoClient(uri, server_api=pymongo.server_api.ServerApi(
    version="1", strict=True, deprecation_errors=True))
 
