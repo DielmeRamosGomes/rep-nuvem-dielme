9e4beaad52a3e0406f32f43400694f100c4c8d23
DielmeRamosGomes <dielme.ramos@unesp.br>
2025-06-16 11:51:18 -0300

    llp

---
+++ src/segundo-bimestre/llp/s14/llp-s14a1/for-matriz.py
@@ -0,0 +1,15 @@
+import random
+matriz_aleatoria = [[random.randint(0, 100) for _ in range(3)] for _ in range(3)]
+matriz = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+def exibir_matriz(matriz):
+    for linha in matriz:
+        for coluna in linha:
+            print(f"matriz_aleatoria[{matriz.index(linha)}][{linha.index(coluna)}] = {matriz[matriz.index(linha)][linha.index(coluna)]}", end=" ")
+            print('\n')
+
+exibir_matriz(matriz)
+
+# 1 2 3
+# 4 5 6
+# 7 8 9
