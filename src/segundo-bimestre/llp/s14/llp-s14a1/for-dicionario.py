8aa40c49e6f71957117c89e692c28044619fe019
DielmeRamosGomes <dielme.ramos@unesp.br>
2025-06-16 10:49:59 -0300

    llp

---
+++ src/segundo-bimestre/llp/s14/llp-s14a1/for-dicionario.py
@@ -0,0 +1,9 @@
+usuarios = [
+    {"id": 1, "nome": "João", "idade": 30},
+    {"id": 2, "nome": "Maria", "idade": 25},
+    {"id": 3, "nome": "Pedro", "idade": 28}
+]
+
+for usuario in usuarios:
+    print(f"ID: {usuario['id']}  Nome: {usuario['nome']} Idade: {usuario['idade']}")
+
