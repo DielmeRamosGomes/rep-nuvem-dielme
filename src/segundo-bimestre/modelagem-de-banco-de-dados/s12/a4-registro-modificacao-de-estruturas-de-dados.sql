ceef38dbaba725876419e756e59c1aa328f1b1a1
DielmeRamosGomes <dielme.ramos@unesp.br>
2025-06-24 09:08:13 -0300

    modelagem

--- src/segundo-bimestre/modelagem-de-banco-de-dados/s12/a4-registro-modificacao-de-estruturas-de-dados.sql
+++ src/segundo-bimestre/modelagem-de-banco-de-dados/s12/a4-registro-modificacao-de-estruturas-de-dados.sql
@@ -3,7 +3,7 @@
 create database if not exists techfix;
 
 CREATE TABLE
-    clientes (
+    techfix.clientes (
         id_cliente INT AUTO_INCREMENT PRIMARY KEY,
         nome VARCHAR(50) NOT NULL,
         email VARCHAR(100) NOT NULL,
@@ -12,7 +12,7 @@ CREATE TABLE
     );
 
 CREATE TABLE
-    produtos (
+    techfix.produtos (
         id_produto INT AUTO_INCREMENT PRIMARY KEY,
         nome_produto VARCHAR(100) NOT NULL,
         preco DECIMAL(10, 2) NOT NULL,
@@ -21,10 +21,29 @@ CREATE TABLE
     );
 
 CREATE TABLE
-    pedidos (
+    techfix.pedidos (
         id_pedido INT AUTO_INCREMENT PRIMARY KEY,
         id_cliente INT,
         data_pedido DATE,
         valor_total DECIMAL(10, 2),
         FOREIGN KEY (id_cliente) REFERENCES clientes (id_cliente)
     );
+
+select * from techfix.clientes;
+
+ALTER TABLE techfix.clientes
+ADD COLUMN telefone_secundario VARCHAR(15);
+
+ALTER TABLE techfix.produtos 
+DROP COLUMN fabricante;
+
+ALTER TABLE techfix.pedidos 
+MODIFY COLUMN data_pedido DATETIME;
+
+ALTER TABLE techfix.produtos 
+ADD COLUMN peso DECIMAL(5,2), 
+ADD COLUMN dimensao VARCHAR(50);
+
+ALTER TABLE techfix.clientes 
+MODIFY COLUMN nome VARCHAR(100);
+
