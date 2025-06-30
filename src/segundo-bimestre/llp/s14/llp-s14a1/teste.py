55aab3f3e1e0e2dd67b525393a670ac4cb4ca07e
DielmeRamosGomes <dielme.ramos@unesp.br>
2025-06-30 10:47:23 -0300

    soma

--- src/segundo-bimestre/llp/s14/llp-s14a1/teste.py
+++ src/segundo-bimestre/llp/s14/llp-s14a1/teste.py
@@ -1,25 +1,7 @@
-def calcular_multa(tipo_livro, dias_atraso):
-    
-    categorias = {
-        'ficção': lambda dias: dias * 0.50,
-        'não ficção': lambda dias: dias * 0.60,
-        'referência': lambda dias: 0.00
-    }
-    
+def soma(a , b):
+    return a + b
 
-    if tipo_livro in categorias:
-        return categorias[tipo_livro](dias_atraso)
-    else:
-        
-        return "Categoria de livro inválida."
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+print(f"A soma de {numero1} + {numero2} = {soma(numero1, numero2)}")
 
-
-tipo_livro = input("Digite a categoria do livro (ficção, não ficção, referência): ").strip().lower()
-dias_atraso = int(input("Digite o número de dias de atraso: "))
-
-multa = calcular_multa(tipo_livro, dias_atraso)
-
-if isinstance(multa, float):
-    print(f"A multa é: R$ {multa:.2f}")
-else:
-    print(multa)
