10ce808ce088f12e902ceef4fab8d8fee6376a24
DielmeRamosGomes <dielme.ramos@unesp.br>
2025-08-14 06:30:16 -0300

    grid

---
+++ src/terceiro-bimestre/pfe/s15/a1/jogo-da-velha/js/script.js
@@ -0,0 +1,10 @@
+const paragrafoCelula1 = document.querySelector('#paragrafo-celula1');
+const celula1 = document.querySelector('.celula1');
+const gridContainer = document.querySelector('.grid-container');
+let Game = [] 
+
+celula1.addEventListener('click', function(event) {
+    paragrafoCelula1.textContent = 'X';
+});
+
+
