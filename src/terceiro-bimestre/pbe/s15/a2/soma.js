71cd44d0e6d82570c06d613c5d2a8201a1947eab
DielmeRamosGomes <dielme.ramos@unesp.br>
2025-08-17 19:31:48 -0300

    pbe

---
+++ src/terceiro-bimestre/pbe/s15/a2/soma.js
@@ -0,0 +1,8 @@
+function sum(number1, number2) {
+    return number1 + number2;
+}
+
+let num1 = 10;
+let num2 = 20;
+console.log(`A soma de ${num1} + ${num2} = ${sum(num1, num2)}`);
+
---
+++ src/terceiro-bimestre/pbe/s15/a3/multiplicacao.js
@@ -0,0 +1,8 @@
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+
+let num1 = 10;
+let num2 = 20;
+console.log(`A soma de ${num1} * ${num2} = ${multiply(num1, num2)}`);
+
