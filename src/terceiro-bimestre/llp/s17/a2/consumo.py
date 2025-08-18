5d7055e8d3671f7409a36603880505897a58372d
DielmeRamosGomes <dielme.ramos@unesp.br>
2025-08-18 07:44:00 -0300

    pbe

--- src/terceiro-bimestre/llp/s17/a2/consumo.py
+++ src/terceiro-bimestre/llp/s17/a2/consumo.py
@@ -1,5 +1,5 @@
 distancia_percorridaX = float(input("Digite a distância percorrida em km: "))
 total_combustivelY= float(input("Digite o total de combustível gasto em litros: "))
 media = (distancia_percorridaX/total_combustivelY)
-print(f"O Consumo médio do automóvel é = {media:.3f} km/l")
+print(f"O Consumo médio do automóvel é = {media: .3f} km/l")
 
