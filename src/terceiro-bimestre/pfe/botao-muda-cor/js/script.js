667f1659e9eacad8a877d2b21506910b2ca26aa9
DielmeRamosGomes <dielme.ramos@unesp.br>
2025-08-28 22:12:04 -0300

    teste

--- src/terceiro-bimestre/pfe/botao-muda-cor/js/script.js
+++ src/terceiro-bimestre/pfe/botao-muda-cor/js/script.js
@@ -1,11 +1,14 @@
 const buttonCor = document.querySelector('.button-cor');
 const colorBox = document.querySelector('#colorBox');
 const pNewColor = document.querySelector('#p-new-color');
+const h1Title = document.querySelector('.h1-title');
 
 buttonCor.addEventListener('click', () => {
     const novaCor = geraCorAleatoria();
     colorBox.style.backgroundColor = novaCor;
     pNewColor.textContent = `Cor: ${novaCor}`;
+    h1Title.style.color = novaCor;
+    buttonCor.style.backgroundColor = novaCor;
 });
 
 buttonCor.addEventListener('mouseover', () => {
