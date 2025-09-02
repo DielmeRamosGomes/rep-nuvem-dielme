c50d9846baf14e8d1209ce73679fb36b7399f913
DielmeRamosGomes <dielme.ramos@unesp.br>
2025-09-02 19:25:28 -0300

    pfe

---
+++ src/terceiro-bimestre/llp/s19/a2/fibonacci-recursivo.py
@@ -0,0 +1,13 @@
+def fibonacci(num):
+    if num == 0:
+        return 0
+    elif num == 1:
+        return 1
+    else:
+        return fibonacci(num-1) + fibonacci(num-2)
+    
+n = int(input("Digite um número inteiro positivo: "))
+while n < 0:
+    n = int(input("Número inválido. Digite um número inteiro positivo: "))
+print(f"O {n}º número da sequência de Fibonacci é: {fibonacci(n)}")  
+
--- src/terceiro-bimestre/llp/s20/a1/do-while.py
+++ src/terceiro-bimestre/llp/s20/a1/do-while.py
@@ -1,14 +1,6 @@
-
-def fibonacci(num):
-    if num == 0:
-        return 0
-    elif num == 1:
-        return 1
-    else:
-        return fibonacci(num-1) + fibonacci(num-2)
-    
-n = int(input("Digite um número inteiro positivo: "))
-while n < 0:
-    n = int(input("Número inválido. Digite um número inteiro positivo: "))
-print(f"O {n}º número da sequência de Fibonacci é: {fibonacci(n)}")  
-
+cont = 0
+while True:
+    print(cont)
+    cont += 1
+    if cont > 10:
+        break
