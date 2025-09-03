74900b664578aeffa8b2a099ab87e55a66664f89
DielmeRamosGomes <dielme.ramos@unesp.br>
2025-09-02 23:02:25 -0300

    llp

---
+++ src/terceiro-bimestre/llp/s20/a1/do-while.js
@@ -0,0 +1,7 @@
+let cont = -1;
+do {
+    cont = cont + 1;
+    console.log(cont);
+} while (cont < 5);
+
+
--- src/terceiro-bimestre/llp/s20/a1/do-while.py
+++ src/terceiro-bimestre/llp/s20/a1/do-while.py
@@ -2,5 +2,5 @@ cont = 0
 while True:
     print(cont)
     cont += 1
-    if cont > 10:
+    if cont > 5:
         break
