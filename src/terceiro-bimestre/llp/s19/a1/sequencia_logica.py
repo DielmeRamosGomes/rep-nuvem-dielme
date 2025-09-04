a25e854007188781b1c121fce158c52a9e8ba57f
DielmeRamosGomes <dielme.ramos@unesp.br>
2025-09-04 09:49:46 -0300

    llp

--- src/terceiro-bimestre/llp/s19/a1/sequencia_logica.py
+++ src/terceiro-bimestre/llp/s19/a1/sequencia_logica.py
@@ -1,5 +1,5 @@
 numero_N = int(input("Digite um número N [2, 999]: "))
-while numero_N <= 1 or numero_N >= 1000:
+while numero_N < 1 or numero_N > 1000:
     numero_N = int(input("Número inválido! Digite um número N [2, 999]: "))
 for i in range(1, numero_N + 1):
     print(i, i**2, i**3)
