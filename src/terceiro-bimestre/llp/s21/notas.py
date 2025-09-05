acb31455ef91d0c4def9bc1226c39e35e4baf9dc
DielmeRamosGomes <dielme.ramos@unesp.br>
2025-09-05 09:14:53 -0300

    teste

--- src/terceiro-bimestre/llp/s21/notas.py
+++ src/terceiro-bimestre/llp/s21/notas.py
@@ -1,17 +1,17 @@
 notas = [85, 92, 78, 90, 88]
 
 # nota do terceiro aluno
-print(f"Notas[2] = {notas[2]}")
+#print(f"Notas[2] = {notas[2]}")
 
 # acesso negativo
-# print(f"Notas[4] = {notas[-1]}")
+#print(f"Notas[4] = {notas[-1]}")
 
 # acessando penúltimo aluno
-# print(f"Notas[3] = {notas[-2]}")  
+#print(f"Notas[3] = {notas[-2]}")  
 
 # vetor misto
-# lista_mista = ['a', 2.3, True, 'Python']
-# print(f"Lista mista: {lista_mista}")
+#lista_mista = ['a', 2.3, True, 'Python']
+#print(f"Lista mista: {lista_mista}")
 
 # acessar um indice fora do limite resulta em IndexError 
 # print(f"Notas[5] = {notas[5]}")
