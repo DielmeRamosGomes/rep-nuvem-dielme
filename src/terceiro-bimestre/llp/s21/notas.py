a0f3b60c4c6294507316c9f75638a9c418751e89
DielmeRamosGomes <dielme.ramos@unesp.br>
2025-09-04 21:11:41 -0300

    llp

---
+++ src/terceiro-bimestre/llp/s21/notas.py
@@ -0,0 +1,17 @@
+notas = [85, 92, 78, 90, 88]
+
+# nota do terceiro aluno
+print(f"Notas[2] = {notas[2]}")
+
+# acesso negativo
+# print(f"Notas[4] = {notas[-1]}")
+
+# acessando penúltimo aluno
+# print(f"Notas[3] = {notas[-2]}")  
+
+# vetor misto
+# lista_mista = ['a', 2.3, True, 'Python']
+# print(f"Lista mista: {lista_mista}")
+
+
+
