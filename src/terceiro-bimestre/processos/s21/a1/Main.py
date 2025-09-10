9807c57cad5cdb49b2a4020cf7b5ae2e78f59942
DielmeRamosGomes <dielme.ramos@unesp.br>
2025-09-10 11:14:00 -0300

    processos

--- src/terceiro-bimestre/processos/s21/a1/Main.py
+++ src/terceiro-bimestre/processos/s21/a1/Main.py
@@ -1,15 +1,15 @@
 from ContaBancaria import ContaBancaria
-#from ContaCorrente import ContaCorrente
+from ContaCorrente import ContaCorrente
 
 if __name__=="__main__":
     conta1 = ContaBancaria("0002", 200)
     conta1.consultar_saldo()
 
-    #conta2 = ContaCorrente("0001")
-    #print("Saldo antes de depositar dinheiro")
-    #conta2.consultar_saldo()
-    #conta2.depositar(100)
-    #print("Saldo depois de depositar R$ 100.00")
-    #conta2.consultar_saldo()
+    conta2 = ContaCorrente("0001")
+    print("Saldo antes de depositar dinheiro")
+    conta2.consultar_saldo()
+    conta2.depositar(100)
+    print("Saldo depois de depositar R$ 100.00")
+    conta2.consultar_saldo()
 
     
