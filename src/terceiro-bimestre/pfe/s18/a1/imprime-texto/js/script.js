64f09dddc8a718de11fe38c85d5268b4a377eae2
DielmeRamosGomes <dielme.ramos@unesp.br>
2025-09-10 18:29:58 -0300

    pfe

--- src/terceiro-bimestre/pfe/s18/a1/imprime-texto/js/script.js
+++ src/terceiro-bimestre/pfe/s18/a1/imprime-texto/js/script.js
@@ -2,7 +2,17 @@ const btnEnviar = document.querySelector('#btn-enviar');
 const paragrafoFrase = document.querySelector('#paragrafo-frase');
 const inputFrase = document.querySelector('#input-frase');
 
-btnEnviar.addEventListener('click', () =>{
+btnEnviar.addEventListener('click', () => {
+    let novaCor = corAleatoria();
+    paragrafoFrase.style.backgroundColor = novaCor;
     paragrafoFrase.innerText = inputFrase.value
 });
 
+function corAleatoria() {
+    let cor = '#';
+    let algarismos = '0123456789ABCDEF';
+    for (let i = 0 ; i < 6 ; i++) {
+        cor += algarismos[Math.floor(Math.random() * 16)]
+    }
+    return cor;
+}
