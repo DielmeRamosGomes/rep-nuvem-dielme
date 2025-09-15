b44d463bc86ccf339580cfc668e3a79530c393a8
DielmeRamosGomes <dielme.ramos@unesp.br>
2025-09-15 11:46:03 -0300

    llp

--- src/terceiro-bimestre/llp/teste/teste.py
+++ src/terceiro-bimestre/llp/teste/teste.py
@@ -1,6 +1,24 @@
-for i in range(1, 11):
-    print(i)
+import random
 
+def lista_aleatoria(num):
+    lista = []
+    for i in range(num):
+        lista.append(random.randint(1, 100))
+    print(f"Lista desordenada: {lista}")
+    return lista
 
+def ordenar(lista):
+    lista.sort()
+    print(f"Lista ordenada = {lista}")
 
+def executar():
+    while True:
+        num = int(input("Digite a quatidade de numeros da lista: "))
+        lista = lista_aleatoria(num)
+        ordenar(lista)
+        continua = input("Deseja continuar?[s, n]: ").lower()
+        if continua == 'n':
+            break         
+
+executar()
 
